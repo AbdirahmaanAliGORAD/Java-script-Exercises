d2c63fc9050c8efda7027a62e8c60224ca987318
AbdirahmaanAliGOROD <abdirahmaanali0025@gmail.com>
2026-02-25 22:15:12 -0800

    waa EX-4

---
+++ Java script Ex4/main.js
@@ -0,0 +1,18 @@
+function add(a,b) {
+    console.log(a+b)
+}
+
+add(10,10);
+add(50,50);
+
+function mult(c,d) {
+    console.log(c*d)
+}
+
+mult(10,10)
+
+const success = function (name) {
+    console.log("hello" + "  "  +name)
+}
+
+success ("Abdirahmaan")
