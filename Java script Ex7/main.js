da87169e9f714da3c475d48320d9dd22c1cdb002
AbdirahmaanAliGOROD <abdirahmaanali0025@gmail.com>
2026-02-28 03:01:31 -0800

    waa EX-7

---
+++ Java script Ex7/main.js
@@ -0,0 +1,20 @@
+let collors = ["blue", "green",  "black",]
+console.log(collors)
+
+console.log("after push")
+collors.push("yellow")
+console.log(collors)
+console.log(collors.length)
+
+console.log("after pop")
+collors.pop()
+console.log(collors)
+console.log(collors.length)
+
+console.log("aftert shift")
+collors.shift()
+console.log(collors)
+
+console.log("after unshift")
+collors.unshift("red")
+console.log(collors)
