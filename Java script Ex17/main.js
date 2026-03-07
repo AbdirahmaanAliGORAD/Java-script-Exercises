be01c694fea3447ede7eab90b10ec45fff8493d1
AbdirahmaanAliGOROD <abdirahmaanali0025@gmail.com>
2026-03-07 03:20:30 -0800

    waa EX-17

---
+++ Java script Ex17/main.js
@@ -0,0 +1,14 @@
+let temprature =0;
+
+if(temprature <= 0){
+    console.log("very cold");
+
+}else if(temprature <= 5){
+    console.log("cold")
+
+}else if(temprature <= 25){
+    console.log("warm")
+}else{
+    console.log("ilaahay ha idiin sakhiro kuleel ka jira");
+}
+
