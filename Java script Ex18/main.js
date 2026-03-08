a17580b68883cebe132f9b2427e93601088c8551
AbdirahmaanAliGOROD <abdirahmaanali0025@gmail.com>
2026-03-08 03:44:14 -0700

    waa EX-18

---
+++ Java script Ex18/main.js
@@ -0,0 +1,12 @@
+// let score =60;
+
+
+// const Grade = score >= 60 ? "you are passed" : "you are failed"
+// console.log(Grade)
+
+
+let score = 60;
+
+const Grade = score <=60 ? "you are passed" : "you are failed"
+console.log(Grade)   
+
