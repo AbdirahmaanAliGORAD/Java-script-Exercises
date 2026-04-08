3fe3d922bfd8fc91ca63a3833c4d0a8c4f5b59d5
AbdirahmaanAliGORAD <abdirahmaanali0025@gmail.com>
2026-04-08 14:38:24 +0300

    waa EX-33

---
+++ Java script Ex33/main.js
@@ -0,0 +1,14 @@
+const dugsiiye = document.querySelector('#dugsiiye');
+console.log(dugsiiye);
+
+const dev = document.querySelector('.dev');
+console.log(dev);
+
+
+function changeContent (){
+    dugsiiye.textContent = "allaahu Akbar";
+}
+
+function changeElement (){
+    dev.innerHTML = 'AL XAMULILAAHI';
+}
